887c486b49d23772cb62a0800a367dc8a6476a96
cfaiexplorayaprende-bo <cfaiexplorayaprende@gmail.com>
2026-01-19 23:33:15 -0500

    Update firebase-config.js
    Actualizar configuración de Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,21 @@
-﻿const firebaseConfig = {
-  apiKey: "AIzaSyDxxxxxx...",
-  authDomain: "explora-y-aprende.firebaseapp.com",
-  projectId: "explora-y-aprende",
-  storageBucket: "explora-y-aprende.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcd1234"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQWliQQRvSxNOWyDkr9nww3lJBUS1YgcA",
+  authDomain: "explora-y-aprende-573dc.firebaseapp.com",
+  projectId: "explora-y-aprende-573dc",
+  storageBucket: "explora-y-aprende-573dc.firebasestorage.app",
+  messagingSenderId: "892202090791",
+  appId: "1:892202090791:web:6056bae61eff5675666902",
+  measurementId: "G-13VJYSLSLH"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
